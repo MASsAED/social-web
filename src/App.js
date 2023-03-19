2c982ea9260b9411d4df1aed24366cc3b2558696
masoud saed <masoudsaed918@gmail.com>
2023-03-19 21:36:57 +0000

    first commit-set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+      <Button variant="text">Text</Button>
+      <Button startIcon={<SettingsIcon/>} variant="contained" color="secondary" size="small">Contained</Button>
+      <Button variant="outlined" disabled>Outlined</Button>
+      </div>
   );
 }
 
