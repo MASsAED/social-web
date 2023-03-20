b7514425a576a86af164ff6d3a22781a0cb6bc05
masoud saed <masoudsaed918@gmail.com>
2023-03-20 01:08:45 +0000

    Added Mui tags With diffrent abilites for diffret cases

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
+import { Pets } from '@mui/icons-material'
+import { AppBar, styled, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+
+const StyledToolbar = styled(Toolbar)({
+display:"flex",
+justifyContent: "space-between"  
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white"
+}))
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{display:{xs:"none", sm:"block"}}}>LAMA DEV</Typography>
+        <Pets sx={{display:{xs:"block", sm:"none"}}} />
+        <Search>Search</Search>
+      </StyledToolbar>
+    </AppBar>
   )
 }
 
